0c45adea52ed3a9fd9dcc7dd700b37dc12f25730
Enrique Ramirez <nrqrmz@gmail.com>
2022-11-18 18:26:59 -0600

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+15.times do |i|
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(3..5)
+  )
+  puts "Restaurant #{i + 1} created!"
+end
+
+puts "Finished!"
